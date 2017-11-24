eb44659e3a60a5898b7a2350c294a5055b75034e
rankitbishnoi <rankitbishnoi@users.noreply.github.com>
2017-11-24 10:36:30 +0530

    edit

--- angular/services/getdata.js
+++ angular/services/getdata.js
@@ -9,7 +9,7 @@ myapp.service('getdata', ['$http', function($http){
 		$http.get('https://www.anapioficeandfire.com/api/books').then(function successCallback(response){
           // this callback will be called asynchronously
           // when the response is available
-          self.data.houses = response.data.map(function(house){ return house; };
+          self.data.houses = response.data.map(function(house){ return house; });
       }, function errorCallback(response) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
@@ -21,7 +21,7 @@ myapp.service('getdata', ['$http', function($http){
       	$http.get('https://www.anapioficeandfire.com/api/books').then(function successCallback(response){
           // this callback will be called asynchronously
           // when the response is available
-          self.data.books = response.data.map(function(book){ return book; };
+          self.data.books = response.data.map(function(book){ return book; });
       }, function errorCallback(response) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
@@ -34,7 +34,7 @@ myapp.service('getdata', ['$http', function($http){
       		$http.get('https://www.anapioficeandfire.com/api/characters?page='+i+'&pageSize=10').then(function successCallback(response){
           // this callback will be called asynchronously
           // when the response is available
-          self.data.characters = response.data.map(function(character){ return character; };
+          self.data.characters = response.data.map(function(character){ return character; });
       }, function errorCallback(response) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
