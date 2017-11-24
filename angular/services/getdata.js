497724594b1bf1b6a98919baba8d00a8f0d54622
rankitbishnoi <rankitbishnoi@users.noreply.github.com>
2017-11-24 10:50:17 +0530

    edit

--- angular/services/getdata.js
+++ angular/services/getdata.js
@@ -6,7 +6,7 @@ myapp.service('getdata', ['$http', function($http){
 		books : []
 	}
 	self.loadAllhouses = function() {
-		$http.get('https://www.anapioficeandfire.com/api/books').then(function successCallback(response){
+		$http.get('https://www.anapioficeandfire.com/api/houses').then(function successCallback(response){
           // this callback will be called asynchronously
           // when the response is available
           self.data.houses = response.data.map(function(house){ return house; });console.log(response);
@@ -22,7 +22,7 @@ myapp.service('getdata', ['$http', function($http){
   	$http.get('https://www.anapioficeandfire.com/api/books').then(function successCallback(response){
           // this callback will be called asynchronously
           // when the response is available
-          self.data.books = response.data.map(function(book){ return book; });
+          self.data.books = response.data.map(function(book){ return book; });console.log(response);
       }, function errorCallback(response) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
@@ -36,7 +36,7 @@ myapp.service('getdata', ['$http', function($http){
   		$http.get('https://www.anapioficeandfire.com/api/characters?page='+i+'&pageSize=10').then(function successCallback(response){
           // this callback will be called asynchronously
           // when the response is available
-          self.data.characters = response.data.map(function(character){ return character; });console.log(response);
+          self.data.characters = response.data.map(function(character){ return character; });
       }, function errorCallback(response) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
