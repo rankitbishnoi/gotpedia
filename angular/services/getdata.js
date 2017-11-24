8acb6241fa9bf9187b5bad0a664a16825dac84a7
rankitbishnoi <rankitbishnoi@users.noreply.github.com>
2017-11-24 10:56:30 +0530

    edit

--- angular/services/getdata.js
+++ angular/services/getdata.js
@@ -6,20 +6,23 @@ myapp.service('getdata', ['$http', function($http){
 		books : []
 	}
 	self.loadAllhouses = function() {
-		$http.get('https://www.anapioficeandfire.com/api/houses').then(function successCallback(response){
+		for (var i = 1; i <= 45; i++) {
+			$http.get('https://www.anapioficeandfire.com/api/houses').then(function successCallback(response){
           // this callback will be called asynchronously
           // when the response is available
-          self.data.houses = response.data.map(function(house){ return house; });console.log(response);
+          self.data.houses = response.data.map(function(house){ return house; });
       }, function errorCallback(response) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
           alert("some error occurred. Check the console.");
           console.log(response);
       })
-  };
+		}
+	};
 
-  self.loadAllbooks = function() {
-  	$http.get('https://www.anapioficeandfire.com/api/books').then(function successCallback(response){
+	self.loadAllbooks = function() {
+		for (var i = 1; i <= 2; i++) {
+			$http.get('https://www.anapioficeandfire.com/api/books').then(function successCallback(response){
           // this callback will be called asynchronously
           // when the response is available
           self.data.books = response.data.map(function(book){ return book; });console.log(response);
@@ -29,11 +32,12 @@ myapp.service('getdata', ['$http', function($http){
           alert("some error occurred. Check the console.");
           console.log(response);
       })
-  };
+		}
+	};
 
-  self.loadAllcharacters = function() {
-  	for (var i = 1; i <= 214; i++) {
-  		$http.get('https://www.anapioficeandfire.com/api/characters?page='+i+'&pageSize=10').then(function successCallback(response){
+	self.loadAllcharacters = function() {
+		for (var i = 1; i <= 214; i++) {
+			$http.get('https://www.anapioficeandfire.com/api/characters?page='+i+'&pageSize=10').then(function successCallback(response){
           // this callback will be called asynchronously
           // when the response is available
           self.data.characters = response.data.map(function(character){ return character; });
@@ -43,11 +47,11 @@ myapp.service('getdata', ['$http', function($http){
           alert("some error occurred. Check the console.");
           console.log(response);
       })
-  }
-};
+		}
+	};
 
-  console.log(self.data);
-  self.loadAllcharacters();
-  self.loadAllbooks();
-  self.loadAllhouses();
+	console.log(self.data);
+	self.loadAllcharacters();
+	self.loadAllbooks();
+	self.loadAllhouses();
 }])
