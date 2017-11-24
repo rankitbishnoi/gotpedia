226c8bfc314a98903d9296d0174bc9701970eb45
rankitbishnoi <rankitbishnoi@users.noreply.github.com>
2017-11-24 15:38:32 +0530

    edit

--- angular/controller/typeaheadCtrl.js
+++ angular/controller/typeaheadCtrl.js
@@ -1,6 +1,6 @@
 myapp.controller('TypeaheadCtrl',["getdata", function(getdata){
 	var self = this;
-	self.data = getdata.self.load();
+	self.data = getdata.load();
 
 	self.selected = undefined;
 	self.search = self.data.books;
