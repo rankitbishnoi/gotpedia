f76d1577cfbbddc83b7eb0b2f3e914f3d28869ff
rankitbishnoi <rankitbishnoi@users.noreply.github.com>
2017-11-24 17:23:39 +0530

    edit

--- angular/controller/searchController.js
+++ angular/controller/searchController.js
@@ -1,5 +1,6 @@
 myapp.controller('searchController',["$stateParams",'getdata', function($stateParams,getdata){
 	var self = this;
 	self.query = $stateParams.query;
-	
+	self.action = "Books";
+	self.filter = "FilterBy";
 }])
