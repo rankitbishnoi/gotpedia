abd00144a2117b2bc2a0319e6bad45aabcf70570
rankitbishnoi <rankitbishnoi@users.noreply.github.com>
2017-11-26 08:54:42 +0530

    edit

--- angular/controller/homeController.js
+++ angular/controller/homeController.js
@@ -1,36 +1,22 @@
 myapp.controller('homeController', ["getdata", function(getdata){
 	var self = this;
 	self.action = "Books";
-	self.filter = "FilterBy";
-	self.filters = [];
-	self.actionDone = "Search By";
 	self.data = getdata.load();
 	self.selected = undefined;
 
 	self.buttonName = function(name) {
 		self.action = name;
-		if (name === "Books") {
-			self.filters = ["name","authors","isbn"];
-			self.actionDone = "Search By";			
+		if (name === "Books") {		
 			self.search = self.data.books;
-		}else if (name === "Characters") {
-			self.filters = ["gender","culture","isAlive"];
-			self.actionDone = "Filter By";			
+		}else if (name === "Characters") {		
 			self.search = self.data.characters;
-		}else if (name === "Houses") {
-			self.filters = ["region"];
-			self.actionDone = "Filter By";			
+		}else if (name === "Houses") {		
 			self.search = self.data.houses;
 		} else {
 
 		};
 	}
 
-
-
-	self.filterName = function(index) {
-		self.filter = self.filters[index];
-	}
 
 
 	self.status = {
