57be89e05d3371027fcfb5f58b8c0bc7619b5b24
rankitbishnoi <rankitbishnoi@users.noreply.github.com>
2017-12-10 15:31:00 +0530

    edit

--- angular/controller/modelController.js
+++ angular/controller/modelController.js
@@ -6,22 +6,14 @@ myapp.controller('ModaldCtrl',['fetchsingledata','$uibModal','$log','$document',
   $ctrl.animationsEnabled = true;
 
   $ctrl.open = function (baseurl,urltype,size, parentSelector) {
-    var parentElem = parentSelector ? 
-      angular.element($document[0].querySelector('.modal-d ' + parentSelector)) : undefined;
-    var modalInstance = $uibModal.open({
+    $uibModal.open({
       animation: $ctrl.animationsEnabled,
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: 'myModalContent.html',
+      ariaLabelledBy: 'modal-title-bottom',
+      ariaDescribedBy: 'modal-body-bottom',
+      templateUrl: 'stackedModal.html',
+      size: 'sm',
       controller: function($scope) {
-        $scope.items = fetchsingledata.loadData(baseurl,urltype); console.log($scope.items);  
-      },
-      size: size,
-      appendTo: parentElem,
-      resolve: {
-        items: function () {
-          return $ctrl.items;
-        }
+        $scope.item = fetchsingledata.loadData(baseurl,urltype); console.log($scope.items);  
       }
     });
 
